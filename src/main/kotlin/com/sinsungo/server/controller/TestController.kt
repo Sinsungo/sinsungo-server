a01d889380aebd50f15697da2da2742bfa136cdb
Yoongyeong Song <14dnfnfn@gmail.com>
2023-02-25 22:10:12 +0900

    fix: test api mapping simply

--- src/main/kotlin/com/sinsungo/server/controller/TestController.kt
+++ src/main/kotlin/com/sinsungo/server/controller/TestController.kt
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api")
 class TestController {
 
-    @GetMapping("/hello/world")
+    @GetMapping("/hello")
     fun getHello() : String {
         return "Hello-Sinsungo-World";
     }
